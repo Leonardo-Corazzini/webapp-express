12ffe0b33ee2f6ab925db588f9a3c69e9d9e85ec
Leonardo-Corazzini <leocora10@gmail.com>
2025-01-08 16:43:12 +0100

    add  show

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -14,4 +14,30 @@ function index(_, res) {
 }
 
 
-module.exports = { index }
+function show(req, res) {
+    const id = req.params.id
+
+    const sql = `SELECT * FROM movies WHERE id = ?`
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ message: err.message })
+        if (results.length === 0)
+            return res.status(404).json({
+                error: 'Not Found',
+                message: 'Movie not found',
+            })
+
+        const movie = results[0]
+
+        const sql = `SELECT * FROM reviews WHERE movie_id = ?`
+
+        connection.query(sql, [id], (err, results) => {
+            if (err) return res.status(500).json({ message: err.message })
+
+            movie.reviews = results
+            res.json(movie)
+        })
+    })
+}
+
+module.exports = { index, show }
