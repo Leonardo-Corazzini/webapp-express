bb5cac8d5dda7fb70d4c42accd055639e84f1c16
Leonardo-Corazzini <leocora10@gmail.com>
2025-01-08 15:56:18 +0100

    add db connection

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const connection = require('./data/db.js')
 
-app.get('/', (req, res) => {
-    res.send('server accesso')
+
+
+
+app.get('/api/movies', (req, res) => {
+    const sql = 'SELECT * FROM movies';
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        res.json(results);
+    });
 })
 
 
