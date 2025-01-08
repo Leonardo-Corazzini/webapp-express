8b82b98c19aec3ca516553b6749c612d1d555455
Leonardo-Corazzini <leocora10@gmail.com>
2025-01-08 16:52:59 +0100

    add middlewares

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const movieRouter = require('./routes/movieRoutes')
+const notFound = require('./middlewares/notFound')
+const errorHandler = require('./middlewares/errorHandler')
 
 app.get('/', (_, res) => {
     res.send('server in funzione')
@@ -9,7 +11,8 @@ app.get('/', (_, res) => {
 
 app.use('/api/movies', movieRouter)
 
-
+app.use(errorHandler)
+app.use(notFound)
 
 
 
---
+++ middlewares/notFound.js
@@ -0,0 +1,8 @@
+function notFound(_, res) {
+    res.status(404).json({
+        error: 'Not found',
+        message: 'Pagina non trovata',
+    })
+}
+
+module.exports = notFound
